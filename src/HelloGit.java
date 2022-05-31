e7cb70636aa35dfb46d8008e74bbd8a5a529ec6e
Tihamer Sebok <tihamer.sebok@endava.com>
2022-05-31 22:34:11 +0300

    sadList

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloGit {
 
     public static void printHello(){
         System.out.println("Hello Git");
+        List<String> sadList = new ArrayList<>();
+        sadList.add("I");
+        sadList.add("am");
+        sadList.add("so");
+        sadList.add("sad");
+
+        System.out.println(sadList);
     }
 }
