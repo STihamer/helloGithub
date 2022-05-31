b507046eb7b15c3f418cd3c411a85ae6a8584109
Tihamer Sebok <tihamer.sebok@endava.com>
2022-05-31 22:19:39 +0300

    insert the happy lis

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloGit {
 
     public static void printHello(){
         System.out.println("Hello Git");
+        List<String>happyArray = new ArrayList<>();
+        happyArray.add("I");
+        happyArray.add("am");
+        happyArray.add("a");
+        happyArray.add("happy");
+        happyArray.add("list");
+        System.out.println(happyArray);
     }
 }
